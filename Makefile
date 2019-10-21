4de523690ca1f77aa7cd744f9951dc3ef080d4f8
xcd0 <xcd0@xcd0.com>
2019-10-21 18:12:24 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+
+run: build
+	./goprogress 500
+
+build:
+	go build
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	flag.Parse()
+
+	progressVal, _ := strconv.ParseFloat(flag.Arg(0), 64)
+	val := progressVal / 1000
+
+	if progressVal > 1000 {
+		fmt.Println("Finish!")
+		os.Exit(0)
+	}
+
+	if progressVal < 0 {
+		fmt.Println("Error!")
+		os.Exit(1)
+	}
+
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	w, _ := termbox.Size()
+	termbox.Close()
+
+	var width int
+	width = int(w / 3 * 2)
+
+	var progress int
+	progress = int(float64(width) * val)
+
+	f := float64(progress) / float64(width) * 100
+	bar := "  ( "
+	bar += strconv.FormatFloat(f, 'f', 1, 64)
+
+	bar += " % ) ["
+	for i := 0; i < width; i++ {
+		if i < progress {
+			bar += "#"
+		} else if i == progress {
+			bar += ">"
+		} else {
+			bar += " "
+		}
+	}
+	bar += "]"
+
+	fmt.Println(bar)
+}
